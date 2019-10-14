b96da42f888b6301678338bf98542cf2d981022d
Ted Jones <tejones@redhat.com>
2019-10-13 19:18:19 -0500

    Initial commit for spring boot implementation of SAP certification tests for the Fuse camel-sap component.

---
+++ spring-boot/8_2_RFC_Function_Module_Test_Drive/TEST_8_2_18_SWO_INVOKE/src/main/java/org/redhat/certification/fuse/TEST_8_2_18_SWO_INVOKE/CreateRequest.java
@@ -0,0 +1,33 @@
+package org.redhat.certification.fuse.TEST_8_2_18_SWO_INVOKE;
+
+import org.apache.camel.Exchange;
+import org.fusesource.camel.component.sap.SapSynchronousRfcDestinationEndpoint;
+import org.fusesource.camel.component.sap.model.rfc.Structure;
+import org.fusesource.camel.component.sap.model.rfc.Table;
+
+public class CreateRequest {
+
+	public void createRequest(Exchange exchange) throws Exception {
+
+		System.out.println("Here");
+		SapSynchronousRfcDestinationEndpoint endpoint = exchange.getContext().getEndpoint("sap-srfc-destination:quickstartDest:SWO_INVOKE", SapSynchronousRfcDestinationEndpoint.class);
+		Structure request = endpoint.createRequest();
+		System.out.println("Here2");
+		
+		// Retrive Object Handle from Exchange Message
+		Structure objCreateResponse = exchange.getIn().getBody(Structure.class);
+		Integer objectHandle = objCreateResponse.get("OBJECT", Integer.class);
+		
+		// Store Object Handle into Exchange
+		exchange.setProperty("org.redhat.certification.fuse.TEST_8_2_18_SWO_INVOKE.OBJECT_HANDLE", objectHandle);
+		
+		request.put("OBJECT", objectHandle);
+		request.put("VERB", "GetList");
+		request.put("ACCESS", "C");
+		request.put("SYNCHRON", "*");
+		request.put("UNSORTED_CONTAINER", "");
+		
+		exchange.getIn().setBody(request);
+	}
+
+}
